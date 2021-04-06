754150aefd1863916bc405d2ce2c0f54211efb91
Karolina Gomes <63608020+karolinagb@users.noreply.github.com>
2021-04-06 16:08:46 -0300

    Ajuste input senha em Login
    Foi colocado o type password para ocultar a senha e feito um botão de mostrar e ocultar senha, onde o mesmo utiliza um código JS para isso.

---
+++ SaleTheaterTickets/SaleTheaterTickets/wwwroot/js/Views/Account/Login.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function () {
+    $('#showPassword').on('click', function () {
+
+        var passwordField = $('#password');
+        var passwordFielType = passwordField.attr('type');
+
+        if (passwordFielType == 'password') {
+            passwordField.attr('type', 'text');
+            $(this).val('Esconder');
+        }
+        else {
+            passwordField.attr('type', 'password');
+            $(this).val('Mostrar');
+        }
+    });
+});
