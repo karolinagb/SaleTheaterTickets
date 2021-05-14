e17f32ff8d500eb5a1e93e4b31e81ea8a905fca6
Karolina Gomes <63608020+karolinagb@users.noreply.github.com>
2021-05-14 14:35:44 -0300

    [RESOLVIDO] Bug na escolha da forma de pagamento
    Ao escolher a forma de pagamento cartão de débito, o validador entendia que a seleção era nula ou vazia e não aceitava. Verifiquei a documentação do método NotEmpty do Fluent Validation e ele não aceita string vazia, valor null e nem o valor padrão de cada tipo. Como o meu enum de forma de pagamento era inteiro e começava em 0 (valor padrão do int) ele entendia cartão de débito (value=0) como null e não deixava passar. Resolvi isso mudando o Enum para começar com 1. Também defini no select do forma de pagamento o valor "Selecione" como padrão.

--- SaleTheaterTickets/SaleTheaterTickets/Models/Enums/FormOfPayment.cs
+++ SaleTheaterTickets/SaleTheaterTickets/Models/Enums/FormOfPayment.cs
@@ -5,9 +5,9 @@ namespace SaleTheaterTickets.Models
     public enum FormOfPayment : int
     {
         [Display(Name = "Cartão de Débito")]
-        DebitCard = 0,
+        DebitCard = 1,
         [Display(Name = "Cartão de Crédito")]
-        CreditCard = 1,
+        CreditCard = 2,
         [Display(Name = "Dinheiro")]
         Money = 3
     }
